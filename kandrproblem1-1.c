a0853f6a57501bd8551089f0b56fb0071eaeb5fe
TheFloatingString <laurencel2001@gmail.com>
2017-04-21 20:13:42 -0400

    Add files via upload

---
+++ kandrproblem1-1.c
@@ -0,0 +1,13 @@
+/* Prints hello world */
+/* Exercise 1-1       */
+/* This code was partly copied from http://groups.engin.umd.umich.edu/CIS/course.des/cis400/c/hworld.html */
+/* Simply because the programmer tried to use multi-line comments, but messed up real bad. */
+/* [THUMBS UP!] */
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello World \n");
+	printf("\n\n\n");
+}
